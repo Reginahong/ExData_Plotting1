4664384b0481957324670109c5d661e9e68aafbd
Reginahong <regina@bnm.gov.my>
2017-01-08 15:40:03 +0800

    Line Graph for Global Active Power

---
+++ Plot 2.R
@@ -0,0 +1,8 @@
+##Plot 2
+data$Date <- as.Date(data$Date,format='%d/%m/%Y') 
+data$DateTime <- as.POSIXct(paste(data$Date,data$Time))
+if(!file.exists('figures'))dir.create('figures')
+png(filename ='./figures/plot2.png',width = 480,height = 480,units = 'px')
+Sys.setlocale(category = "LC_ALL",locale = "english")
+plot(data$DateTime, data$Global_active_power, xlab = '', ylab = 'Global Active Power (kilowatts)', type = 'l')
+dev.off()
